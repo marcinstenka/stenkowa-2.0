4f196c828e09eb4f2774732325ff4f925d42fb7c
marcinstenka <44028611+marcinstenka@users.noreply.github.com>
2021-08-30 21:50:18 +0200

    2.0 - script changed

--- js/main.js
+++ js/main.js
@@ -5,69 +5,6 @@ const veil = document.querySelector('#veil');
 const lastItem = document.querySelector('#addable');
 const inputs = document.querySelectorAll('input');
 
-const makeTilePossibleToDelete = () => {
-  let deleteXs = document.querySelectorAll('.deleteX');
-  deleteXs.forEach(X => {
-    X.addEventListener('click', () => {
-      X.parentElement.remove();
-      const attr = X.getAttribute('id');
-      function arrayRemove(arr, value) {
-        return arr.filter(function (ele) {
-          return ele.id != value;
-        });
-      }
-      const newList = arrayRemove(tilesList, attr);
-      localStorage.setItem('TILES', JSON.stringify(newList));
-      tilesList = newList;
-    });
-  });
-};
-
-const loadList = array => {
-  array.forEach(item => {
-    generateTile(item.number, item.name, item.link, item.id);
-  });
-};
-const generateTile = (number, name, link, id) => {
-  // creating tile element
-  let tile__container = document.createElement('div');
-  tile__container.classList.add('tile__container');
-
-  let deleteX = document.createElement('div');
-  deleteX.classList.add('deleteX');
-  deleteX.setAttribute('id', id);
-  let X = document.createElement('img');
-  X.setAttribute('src', 'img/x.png');
-  deleteX.appendChild(X);
-  tile__container.appendChild(deleteX);
-
-  let a = document.createElement('a');
-  tile__container.appendChild(a);
-  a.classList.add('tile');
-  a.setAttribute('href', link);
-  let tile__front = document.createElement('div');
-  tile__front.classList.add('tile__front');
-  a.appendChild(tile__front);
-
-  let tile__back = document.createElement('div');
-  tile__back.classList.add('tile__back');
-  a.appendChild(tile__back);
-
-  let front__icon = document.createElement('div');
-  front__icon.classList.add('front__icon');
-  tile__front.appendChild(front__icon);
-
-  let image = document.createElement('img');
-  image.setAttribute('src', `img/icon${number}.png`);
-  front__icon.appendChild(image);
-
-  let front__text = document.createElement('div');
-  front__text.classList.add('front__text');
-  front__text.innerHTML = name;
-  tile__front.appendChild(front__text);
-
-  lastItem.insertBefore(tile__container, lastItem.lastChild.previousSibling);
-};
 const AddNewTile = () => {
   form.style.display = 'none';
   veil.style.display = 'none';
@@ -77,40 +14,52 @@ const AddNewTile = () => {
   // choosing random number for icon
   const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
   const number = random(1, 7);
-  generateTile(number, name, link, id);
-  //add tile to tilesList -> local storage
+  //   generateTile(number, name, link);
+  let getLocalStorage = localStorage.getItem('TILES');
+  getLocalStorage
+    ? (tilesList = JSON.parse(getLocalStorage))
+    : (tilesList = []);
   tilesList.push({
     number,
     name,
     link,
-    id,
   });
-
-  //add item to local storage
   localStorage.setItem('TILES', JSON.stringify(tilesList));
-  localStorage.setItem('lastTileNumber', id);
-  id++;
   //clearing inputs after tile added
   inputs.forEach(input => (input.value = ''));
-  makeTilePossibleToDelete();
+  showTiles();
 };
-//get local storage and check is empty
-let data = localStorage.getItem('TILES');
-if (data) {
-  tilesList = JSON.parse(data);
-  // id = tilesList.length;
-  id = localStorage.getItem('lastTileNumber');
-  loadList(tilesList);
-  makeTilePossibleToDelete();
-} else if ((data = [])) {
-  tilesList = [];
-  id = 0;
-  localStorage.setItem('lastTileNumber', '0');
+
+// main function which refresh tiles
+const showTiles = () => {
+  let getLocalStorage = localStorage.getItem('TILES');
+  getLocalStorage
+    ? (tilesList = JSON.parse(getLocalStorage))
+    : (tilesList = []);
+  let newTiles = '';
+  tilesList.forEach((element, index) => {
+    newTiles += `<div class="tile__container"><div class="deleteX" onclick = "deleteTile(${index})"><img src="img/x.png"></div><a class="tile" href="${element.link}"><div class="tile__front"><div class="front__icon"><img src="img/icon${element.number}.png"></div><div class="front__text">${element.name}</div></div><div class="tile__back"></div></a></div>`;
+  });
+  lastItem.innerHTML = newTiles;
+};
+showTiles();
+
+// each tile has onclick with index
+function deleteTile(index) {
+  let getLocalStorage = localStorage.getItem('TILES');
+  tilesList = JSON.parse(getLocalStorage);
+  tilesList.splice(index, 1);
+  localStorage.setItem('TILES', JSON.stringify(tilesList));
+  showTiles();
 }
 
 //when form submitted
 add.addEventListener('click', AddNewTile);
-
+document.addEventListener('keyup', e => {
+  if (e.code === 'Enter') {
+    AddNewTile();
+  }
+});
 // when clicked "add tile"
 newTile.addEventListener('click', () => {
   form.style.display = 'flex';
--- todo/js/todo.js
+++ todo/js/todo.js
@@ -1,66 +1,5 @@
 const todos = document.querySelector('.todos');
 
-const makeTilePossibleToDelete = () => {
-  let deleteXs = document.querySelectorAll('.todoDelete');
-  deleteXs.forEach(X => {
-    X.addEventListener('click', () => {
-      X.parentElement.remove();
-      const attr = X.getAttribute('id');
-      function arrayRemove(arr, value) {
-        return arr.filter(function (ele) {
-          return ele.id != value;
-        });
-      }
-      const newList = arrayRemove(todosList, attr);
-      localStorage.setItem('TODOS', JSON.stringify(newList));
-      todosList = newList;
-    });
-  });
-};
-
-const loadList = array => {
-  array.forEach(item => {
-    addTodo(item.text, item.date, item.color, item.id);
-  });
-};
-
-const addTodo = (text, date, color, id) => {
-  const todoContainer = document.createElement('div');
-  const todoDelete = document.createElement('div');
-  const img = document.createElement('img');
-  const todo = document.createElement('div');
-  const todoFront = document.createElement('div');
-  const todo__text = document.createElement('h1');
-  const todo__date = document.createElement('h3');
-  const todoBack = document.createElement('div');
-
-  todoContainer.classList.add('todoContainer');
-  todoDelete.classList.add('todoDelete');
-  todo.classList.add('todo-item');
-  todoFront.classList.add('todo-item__front');
-  todo__text.classList.add('todo__text');
-  todo__date.classList.add('todo__date');
-  todoBack.classList.add('todo-item__back');
-
-  todoContainer.appendChild(todoDelete);
-  todoContainer.appendChild(todo);
-  todo.appendChild(todoFront);
-  todoFront.appendChild(todo__text);
-  todoFront.appendChild(todo__date);
-  todo.appendChild(todoBack);
-
-  todoDelete.setAttribute('id', id);
-  let X = document.createElement('img');
-  X.setAttribute('src', 'img/x.png');
-  todoDelete.appendChild(X);
-
-  todo__text.innerHTML = text;
-  todo__date.innerHTML = date;
-  todoBack.style.background = color;
-
-  todos.appendChild(todoContainer);
-};
-
 document.addEventListener('keyup', e => {
   if (e.code === 'Enter') {
     const colorPicker = document.querySelector('.colorPicker');
@@ -69,36 +8,40 @@ document.addEventListener('keyup', e => {
     const text = todoText.value;
     const date = datePicker.value;
     const color = colorPicker.value;
-    addTodo(text, date, color, id);
     //add todo to todosList -> local storage
     todosList.push({
       text,
       date,
       color,
-      id,
     });
     //add item to local storage
     localStorage.setItem('TODOS', JSON.stringify(todosList));
-    localStorage.setItem('lastTodoNumber', id);
-    id++;
 
     todoText.value = '';
     colorPicker.value = '#6558ff';
     today = new Date();
     datePicker.value = today.getDate();
-    makeTilePossibleToDelete();
+    showTodos();
   }
 });
+const showTodos = () => {
+  let getLocalStorage = localStorage.getItem('TODOS');
+  getLocalStorage
+    ? (todosList = JSON.parse(getLocalStorage))
+    : (todosList = []);
+  let newTodos = '';
+  todosList.forEach((element, index) => {
+    newTodos += `<div class="todoContainer"><div class="todoDelete" onclick="deleteTodo(${index})"><img src="img/x.png"></div><div class="todo-item"><div class="todo-item__front"><h1 class="todo__text">${element.text}</h1><h3 class="todo__date">${element.date}</h3></div><div class="todo-item__back" style="background: ${element.color}"></div></div></div>`;
+  });
+  todos.innerHTML = newTodos;
+};
+showTodos();
 
-//get local storage and check is empty
-let data = localStorage.getItem('TODOS');
-if (data) {
-  todosList = JSON.parse(data);
-  id = todosList.length;
-  loadList(todosList);
-  makeTilePossibleToDelete();
-} else if ((data = [])) {
-  todosList = [];
-  id = 0;
-  localStorage.setItem('lastTodoNumber', '0');
+// each todo has onclick with index
+function deleteTodo(index) {
+  let getLocalStorage = localStorage.getItem('TODOS');
+  todosList = JSON.parse(getLocalStorage);
+  todosList.splice(index, 1);
+  localStorage.setItem('TODOS', JSON.stringify(todosList));
+  showTodos();
 }
